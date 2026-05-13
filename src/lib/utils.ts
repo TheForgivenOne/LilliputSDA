7ff1f48076bc02fe856b909cb27ec0d0d60d240b
Farone <successmove000@gmail.com>
2026-05-13 02:11:03 +0000

    tmp: trigger typecheck failure to test end-to-end pipeline

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -247,3 +247,5 @@ const x: number = "this should fail typecheck";
 const x: number = "this should fail typecheck";
 
 const x: number = "this should fail typecheck";
+
+const x: number = "this should fail typecheck";
